55ed3d28f9a082a70c711ae49c1a995577b61a8d
arnyigor <arnyigor@gmail.com>
2022-04-13 01:49:11 +0300

    di refactor

--- domain/src/main/java/com/arny/flightlogbook/domain/common/PreferencesInteractor.kt
+++ domain/src/main/java/com/arny/flightlogbook/domain/common/PreferencesInteractor.kt
@@ -2,34 +2,25 @@ package com.arny.flightlogbook.domain.common
 
 import com.arny.core.CONSTS
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class PreferencesInteractor @Inject constructor(private val provider: PreferencesProvider) :
     IPreferencesInteractor {
-    override fun isAutoImportEnabled(): Boolean {
-        return provider.getPrefBoolean(CONSTS.PREFS.PREF_DROPBOX_AUTOIMPORT_TO_DB, false)
-    }
+    override fun isAutoImportEnabled(): Boolean =
+        provider.getPrefBoolean(CONSTS.PREFS.PREF_DROPBOX_AUTOIMPORT_TO_DB, false)
 
-    override fun getFlightsOrderType(): Int {
-        return provider.getPrefInt(CONSTS.PREFS.PREF_USER_FILTER_FLIGHTS)
-    }
+    override fun getFlightsOrderType(): Int = provider.getPrefInt(CONSTS.PREFS.PREF_USER_FILTER_FLIGHTS)
 
     override fun setOrderType(orderType: Int) {
         provider.setPrefInt(CONSTS.PREFS.PREF_USER_FILTER_FLIGHTS, orderType)
     }
 
-    override fun isAutoExportXLS(): Boolean {
-        return provider.getPrefBoolean(CONSTS.PREFS.AUTO_EXPORT_XLS, false)
-    }
+    override fun isAutoExportXLS(): Boolean = provider.getPrefBoolean(CONSTS.PREFS.AUTO_EXPORT_XLS, false)
 
     override fun setAutoExportXLS(checked: Boolean) {
         provider.setPref(CONSTS.PREFS.AUTO_EXPORT_XLS, checked)
     }
 
-    override fun isSaveLastData(): Boolean {
-        return provider.getPrefBoolean(CONSTS.PREFS.PREF_SAVE_LAST_FLIGHT_DATA, false)
-    }
+    override fun isSaveLastData(): Boolean = provider.getPrefBoolean(CONSTS.PREFS.PREF_SAVE_LAST_FLIGHT_DATA, false)
 
     override fun setSaveLastData(checked: Boolean) {
         provider.setPref(CONSTS.PREFS.PREF_SAVE_LAST_FLIGHT_DATA, checked)
